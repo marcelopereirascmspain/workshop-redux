ddd315456234a8a7e9130c3e74da411430770542
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2016-04-24 12:23:59 +0200

    Rendering nested elements.

--- docs/index.js
+++ docs/index.js
@@ -1,20 +1,50 @@
 var state = 'Hello world!';
 
-function h1(text) {
+function h1(children) {
   return {
     type: 'h1',
-    text: text
+    children: children
+  };
+}
+
+function p(children) {
+  return {
+    type: 'p',
+    children: children
+  };
+}
+
+function div(children) {
+  return {
+    type: 'div',
+    children: children
   };
 }
 
 // app :: state -> UI
 function app(state) {
-  return h1(state);
+  return div([
+    h1(state),
+    p('Render all the things!')
+  ]);
 }
 
 function renderToDOM(component, node) {
-  var domElement = document.createElement(component.type);
-  domElement.textContent = component.text;
+  var type = component.type;
+  var children = component.children;
+
+  var domElement = document.createElement(type);
+
+  // si children es de tipo string
+  // asumimos que es un nodo de texto
+  if (typeof children === 'string') {
+    domElement.textContent = component.children;
+  } else {
+    // llamamos recursivamente renderToDOM
+    // renderizando el el nodo padre
+    children.map(child => renderToDOM(child, domElement));
+  }
+  
   node.appendChild(domElement);
 }
 
