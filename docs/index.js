c5147ad510cac71b2180b727041ead3e655734ae
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2016-04-24 12:03:22 +0200

    Rendering text.

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,12 @@
-console.log('redux workshop');
+var state = 'Hello world!';
+
+// app :: state -> UI
+function app(state) {
+  return state;
+}
+
+function renderToDOM(text, node) {
+  node.textContent = text;
+}
+
+renderToDOM(app(state), document.getElementById('app'));
