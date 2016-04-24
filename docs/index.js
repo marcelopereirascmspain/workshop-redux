f82605ce90d0deabf07c76564cacb42da0743da9
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2016-04-24 12:09:51 +0200

    Rendering dom elements.

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,21 @@
 var state = 'Hello world!';
 
-// app :: state -> UI
-function app(state) {
-  return state;
+function h1(text) {
+  return {
+    type: 'h1',
+    text: text
+  };
 }
 
-function renderToDOM(text, node) {
-  node.textContent = text;
+// app :: state -> UI
+function app(state) {
+  return h1(state);
+}
+
+function renderToDOM(component, node) {
+  var domElement = document.createElement(component.type);
+  domElement.textContent = component.text;
+  node.appendChild(domElement);
 }
 
 renderToDOM(app(state), document.getElementById('app'));
