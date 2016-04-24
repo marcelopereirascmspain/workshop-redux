698de55893db35656cc6ac97897d57e22b0562d0
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2016-04-24 13:31:41 +0200

    Adding events.

---
+++ docs/dom.js
@@ -0,0 +1,41 @@
+var d = tagName => (attrs, children) => {
+  return {
+    type: tagName,
+    attrs: attrs,
+    children: children
+  };
+};
+
+var h1 = d('h1');
+var p = d('p');
+var div = d('div');
+
+function renderToDOM(component, node) {
+  var type = component.type;
+  var attrs = component.attrs;
+  var children = component.children;
+
+  var domElement = document.createElement(type);
+
+  Object.keys(attrs).forEach(key => {
+    var value = attrs[key];
+
+    if (key === 'onClick') {
+      domElement.onclick = value;
+    } else {
+      domElement.setAttribute(key, value);
+    }
+  });
+
+  // si children es de tipo string
+  // asumimos que es un nodo de texto
+  if (typeof children === 'string') {
+    domElement.textContent = component.children;
+  } else {
+    // llamamos recursivamente renderToDOM
+    // renderizando el el nodo padre
+    children.map(child => renderToDOM(child, domElement));
+  }
+  
+  node.appendChild(domElement);
+}
--- docs/index.js
+++ docs/index.js
@@ -1,16 +1,17 @@
 var state = 'Hello world!';
 
-var d = tagName => (attrs, children) => {
-  return {
-    type: tagName,
-    attrs: attrs,
-    children: children
-  };
-};
+function box(props) {
+  return div({}, props.children);
+}
 
-var h1 = d('h1');
-var p = d('p');
-var div = d('div');
+function title(props) {
+  return h1({
+    style: 'color: ' + props.color,
+    onClick: function (e) {
+      alert('clicked!')
+    }
+  }, props.text);
+}
 
 // app :: state -> UI
 function app(state) {
@@ -18,39 +19,6 @@ function app(state) {
     title({ color: 'tomato', text: state}),
     p({}, 'Render all the things!')
   ]});
-}
-
-function box(props) {
-  return div({}, props.children);
-}
-
-function title(props) {
-  return h1({ style: 'color: ' + props.color}, props.text);
-}
-
-function renderToDOM(component, node) {
-  var type = component.type;
-  var attrs = component.attrs;
-  var children = component.children;
-
-  var domElement = document.createElement(type);
-
-  Object.keys(attrs).forEach(key => {
-    var value = attrs[key];
-    domElement.setAttribute(key, value);
-  });
-
-  // si children es de tipo string
-  // asumimos que es un nodo de texto
-  if (typeof children === 'string') {
-    domElement.textContent = component.children;
-  } else {
-    // llamamos recursivamente renderToDOM
-    // renderizando el el nodo padre
-    children.map(child => renderToDOM(child, domElement));
-  }
-  
-  node.appendChild(domElement);
 }
 
 renderToDOM(app(state), document.getElementById('app'));
