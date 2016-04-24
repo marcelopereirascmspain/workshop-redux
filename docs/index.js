0e639310ea2e53cf57c9ca6125a42b7b679611b7
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2016-04-24 12:53:13 +0200

    Add attributes.

--- docs/index.js
+++ docs/index.js
@@ -1,39 +1,48 @@
 var state = 'Hello world!';
 
-function h1(children) {
+function h1(attrs, children) {
   return {
     type: 'h1',
+    attrs: attrs,
     children: children
   };
 }
 
-function p(children) {
+function p(attrs, children) {
   return {
     type: 'p',
+    attrs: attrs,
     children: children
   };
 }
 
-function div(children) {
+function div(attrs, children) {
   return {
     type: 'div',
+    attrs: attrs,
     children: children
   };
 }
 
 // app :: state -> UI
 function app(state) {
-  return div([
-    h1(state),
-    p('Render all the things!')
+  return div({}, [
+    h1({style: 'color:green'}, state),
+    p({}, 'Render all the things!')
   ]);
 }
 
 function renderToDOM(component, node) {
   var type = component.type;
+  var attrs = component.attrs;
   var children = component.children;
 
   var domElement = document.createElement(type);
+  
+  Object.keys(attrs).forEach(key => {
+    var value = attrs[key];
+    domElement.setAttribute(key, value);
+  });
 
   // si children es de tipo string
   // asumimos que es un nodo de texto
